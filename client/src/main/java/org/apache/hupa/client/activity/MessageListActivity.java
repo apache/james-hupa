252453702d9b883b929f168dd7d184d7c924a009
Dongxu <dongxu@apache.org>
2013-09-12 04:42:32 +0000

    add top Deleting... bar when delete message(s)
    git-svn-id: https://svn.apache.org/repos/asf/james/hupa/trunk@1522379 13f79535-47bb-0310-9956-ffa450edef68

--- client/src/main/java/org/apache/hupa/client/activity/MessageListActivity.java
+++ client/src/main/java/org/apache/hupa/client/activity/MessageListActivity.java
@@ -421,6 +421,7 @@ public class MessageListActivity extends AppBaseActivity {
 		}
 	}
 	private void deleteSelectedMessages() {
+		hc.showTopLoading("Deleting...");
 		String fullName = null;
 		if (pc.getWhere() instanceof FolderPlace) {
 			fullName = ((FolderPlace) pc.getWhere()).getToken();
@@ -439,6 +440,7 @@ public class MessageListActivity extends AppBaseActivity {
 			public void onSuccess(DeleteMessageResult response) {
 				antiSelectMessages(display.getSelectedMessages());
 				display.refresh();
+				hc.hideTopLoading();
 				eventBus.fireEvent(new RefreshUnreadEvent());
 			}
 		});
